ae71b98a4e95d176047206a4c10b8ab6d98763ad
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-08-22 12:43:02 +0200

    updated sql migrations

--- sql/001-providers-table.sql
+++ sql/001-providers-table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE providers (
       pseudonym VARCHAR(50) NOT NULL,
-      ura_number VARCHAR(50) NOT NULL ,
+      provider_id VARCHAR(50) NOT NULL ,
       data_domain VARCHAR(100) NOT NULL,
-      PRIMARY KEY (pseudonym, ura_number, data_domain)
+      PRIMARY KEY (pseudonym, provider_id, data_domain)
 );
 
 ALTER TABLE providers OWNER TO localisation;
---
+++ sql/002-provider_id_rename.sql
@@ -0,0 +1,1 @@
+ALTER TABLE providers RENAME COLUMN provider_id TO ura_number;
