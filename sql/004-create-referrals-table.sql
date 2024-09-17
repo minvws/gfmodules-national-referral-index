1e3133a479bb889ca67b6cb0faa24b5bd09b444b
Bas van Driel <contact@basvandriel.nl>
2024-09-17 13:56:58 +0200

    Add migration

---
+++ sql/004-create-referrals-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE referral_request_logs (
+    id SERIAL PRIMARY KEY,
+    ura_number VARCHAR(50) NOT NULL,
+    pseudonym VARCHAR(50) NOT NULL,
+    data_domain VARCHAR(100) NOT NULL,
+    requesting_uzi_number VARCHAR(50) NOT NULL
+);
