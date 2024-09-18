153fbb2df93b5490e95b81e007f954609568f1bd
Bas van Driel <contact@basvandriel.nl>
2024-09-18 10:26:13 +0200

    Fix charlength

--- sql/001-providers-table.sql
+++ sql/001-providers-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE providers (
       pseudonym VARCHAR(50) NOT NULL,
-      provider_id VARCHAR(50) NOT NULL ,
+      provider_id CHAR(9) NOT NULL ,
       data_domain VARCHAR(100) NOT NULL,
       PRIMARY KEY (pseudonym, provider_id, data_domain)
 );
