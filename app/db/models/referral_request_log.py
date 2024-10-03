1549f5fabf1875235a5dd49cb84995ae7d309c6c
Bas van Driel <contact@basvandriel.nl>
2024-10-03 09:18:22 +0200

    Remove comment

--- app/db/models/referral_request_log.py
+++ app/db/models/referral_request_log.py
@@ -12,7 +12,6 @@ class ReferralRequestLogEntry(Base):
     id: Mapped[int] = mapped_column("id", primary_key=True, autoincrement=True)
     endpoint: Mapped[str] = mapped_column("endpoint", String)
 
-    # https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#using-python-enum-or-pep-586-literal-types-in-the-type-map
     request_type: Mapped[ReferralRequestType]
     payload: Mapped[dict[str, Any]] = mapped_column("payload", JSON)
 
