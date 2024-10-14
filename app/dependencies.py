749171f1c60c12b80697a18d61efcc0211064b04
Bas van Driel <contact@basvandriel.nl>
2024-10-14 22:39:53 +0200

    Refactor

--- app/dependencies.py
+++ app/dependencies.py
@@ -6,6 +6,10 @@ from app.db.db import Database
 from app.config import Config, load_default_config
 from app.services.referral_service import ReferralService
 from app.services.pseudonym_service import PseudonymService
+from app.services.ura_number_finder import (
+    ConfigOverridenURANumberFinder,
+    StarletteRequestURANumberFinder,
+)
 
 
 def get_default_config() -> Config:
@@ -27,4 +31,14 @@ def get_pseudonym_service() -> PseudonymService:
 def authenticated_ura(
     request: Request, config: Config = Depends(get_default_config)
 ) -> UraNumber:
-    return resolve_authenticated_ura_number(request, config)
+    # I would also not use the injection framework for the URA number resolving.
+    # The reason why is that the Starlette request is unknown at the time of object creation.
+    #
+    # The reason why I wouldn't advice an request parameter is because that will break the Liskov Substitution Principle (LSP);
+    # the methods will not have the same signature. Instead, can simplify it here.
+    finder = (
+        ConfigOverridenURANumberFinder()
+        if config.app.override_authentication_ura
+        else StarletteRequestURANumberFinder(request)
+    )
+    return finder.find()
