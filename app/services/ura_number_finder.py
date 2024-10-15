1d77faf5b12c627ecd70a78560f645dfbd5b6d2c
Bas van Driel <contact@basvandriel.nl>
2024-10-15 09:55:24 +0200

    Create constant

--- app/services/ura_number_finder.py
+++ app/services/ura_number_finder.py
@@ -27,6 +27,7 @@ class RequestURANumberFinder(StarletteRequestURANumberFinder):
     _CERT_START = "-----BEGIN CERTIFICATE-----"
     _CERT_END = "-----END CERTIFICATE-----"
 
+    _SSL_CLIENT_CERT_HEADER_NAME = "x-proxy-ssl_client_cert"
     _cert: str
 
     def _enforce_cert_newlines(self, cert_data: str) -> str:
@@ -42,12 +43,12 @@ class RequestURANumberFinder(StarletteRequestURANumberFinder):
         return result
 
     def find(self, request: Request) -> UraNumber:
-        if "x-proxy-ssl_client_cert" not in request.headers:
+        if self._SSL_CLIENT_CERT_HEADER_NAME not in request.headers:
             raise HTTPException(
                 status_code=401,
                 detail="Missing client certificate",
             )
-        cert = request.headers["x-proxy-ssl_client_cert"]
+        cert = request.headers[self._SSL_CLIENT_CERT_HEADER_NAME]
 
         formatted_cert = self._enforce_cert_newlines(cert)
         uzi_server = UziServer(verify="SUCCESS", cert=formatted_cert)
